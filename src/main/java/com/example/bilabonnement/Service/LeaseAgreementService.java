5b635d860e27fc324915112020e07a00101faa61
SumayaIAden1 <sumaya73tvs@gmail.com>
2025-05-14 22:18:11 +0200

    tilføjet metoder i leaseAgreement

--- src/main/java/com/example/bilabonnement/Service/LeaseAgreementService.java
+++ src/main/java/com/example/bilabonnement/Service/LeaseAgreementService.java
@@ -6,6 +6,8 @@ import com.example.bilabonnement.Repository.LeaseAgreementRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -18,10 +20,15 @@ public class LeaseAgreementService
         return leaseAgreementRepo.fetchAll();
     }
 
+    /* Tilføjet Logik bag metoden*/
     public void addLeaseAgreement(LeaseAgreement leaseAgreement)
     {
+        double total = calculateTotalPrice(leaseAgreement);
+        leaseAgreement.setTotalPrice(total);
+
         leaseAgreementRepo.addLeaseAgreement(leaseAgreement);
     }
+
 
     public LeaseAgreement findLeaseAgreementById(int id)
     {
@@ -37,4 +44,21 @@ public class LeaseAgreementService
     {
         leaseAgreementRepo.updateLeaseAgreement(id, leaseAgreement);
     }
+
+    /* Metode til beregning af pris --- ChronoUnit.DAYS.between(...): Beregner antal dage mellem to datoer.*/
+    public double calculateTotalPrice(LeaseAgreement leaseAgreement) {
+        LocalDate start = leaseAgreement.getStartDate().toLocalDate();
+        LocalDate end = leaseAgreement.getEndDate().toLocalDate();
+
+        long days = ChronoUnit.DAYS.between(start, end);
+
+        if (days <= 0) {
+            throw new IllegalArgumentException("Slutdato skal være efter startdato");
+        }
+
+        return Math.round((leaseAgreement.getMonthlyPrice() / 30.0) * days * 100.0) / 100.0;
+    }
+
+
+
 }
