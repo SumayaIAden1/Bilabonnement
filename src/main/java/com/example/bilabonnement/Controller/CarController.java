e097b179fa534bcdc92862da60714f9c24e2e39c
Isabellabmj <Isabellajoensen@gmail.com>
2025-05-21 13:08:07 +0200

    tilføjet wrapper til car og create(car)

--- src/main/java/com/example/bilabonnement/Controller/CarController.java
+++ src/main/java/com/example/bilabonnement/Controller/CarController.java
@@ -2,11 +2,14 @@ package com.example.bilabonnement.Controller;
 
 import com.example.bilabonnement.DTO.CarWithModelDTO;
 import com.example.bilabonnement.Model.Car;
+import com.example.bilabonnement.Service.CarModelService;
 import com.example.bilabonnement.Service.CarService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -15,6 +18,9 @@ public class CarController {
 
     @Autowired
     private CarService carService;
+
+    @Autowired
+    private CarModelService carModelService;
 
     // Vis alle biler
     /*@GetMapping("/dataentry/cars")
@@ -26,25 +32,32 @@ public class CarController {
 
     //Isbella - alle biler med model
     @GetMapping("/dataentry/cars")
-    public String getAllCars(Model model) {
-        List<CarWithModelDTO> cars = carService.fetchAllCarsWithModel(); // DTO med car + model
+    public String getAllCars(Model model, HttpSession session) {
+        List<CarWithModelDTO> cars = carService.fetchAllCarsWithModel();
         model.addAttribute("cars", cars);
+        model.addAttribute("user", session.getAttribute("user")); // Skal være med så så brugeren er tilgængelig for Thymeleaf
         return "dataentry/cars";
     }
 
-    // Vis formular til oprettelse af bil
     @GetMapping("/cars/create")
-    public String create(Model model) {
+    public String createCarForm(Model model, HttpSession session) {
         model.addAttribute("car", new Car());
-        return "car/create"; // templates/car/create.html
+        model.addAttribute("models", carModelService.fetchAll());
+        model.addAttribute("user", session.getAttribute("user")); // Gør bruger tilgængelig
+        return "dataentry/create";
     }
 
-    // Gem ny bil
     @PostMapping("/cars/addCar")
-    public String addCar(@ModelAttribute Car car) {
-        carService.addCar(car);
-        return "redirect:/cars";
+    public String addCar(@ModelAttribute Car car, RedirectAttributes redirectAttributes) {
+        carService.addCar(car); // Gem bilen
+
+        // Tilføj en flash-attribut med succesbesked
+        redirectAttributes.addFlashAttribute("successMessage", "Bilen blev oprettet!");
+
+        // Omdirigér til oversigten
+        return "redirect:/dataentry/cars";
     }
+
 
     // Vis én bil
     @GetMapping("/cars/viewOne/{registrationNumber}")
