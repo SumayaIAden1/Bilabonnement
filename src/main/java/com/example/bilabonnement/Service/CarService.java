c31099db7b9757914070721946e1c45a9c0f1932
Isabellabmj <Isabellajoensen@gmail.com>
2025-05-23 12:10:01 +0200

    ændringer til månedlig pris

--- src/main/java/com/example/bilabonnement/Service/CarService.java
+++ src/main/java/com/example/bilabonnement/Service/CarService.java
@@ -6,6 +6,7 @@ import com.example.bilabonnement.Repository.CarRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
